90e509fced02b825897dfef0254ed82161244880
jdbrandt <jdbrandt@stu.naperville203.org>
2016-04-01 18:15:32 -0500

    Probably not working much on VGC, too much annoying math to get perfect

---
+++ vgcsim/Game.java
@@ -0,0 +1,15 @@
+
+public class Game
+{
+    
+    
+    Player p1;
+    Player p2;
+    
+    private enum Weather
+    {
+        CLEAR, SUN, HARSHSUN, RAIN, HARSHRAIN, SAND, HAIL, FOG, MYSTERYAIR
+    }
+    
+}
+    
