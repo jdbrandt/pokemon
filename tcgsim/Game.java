0da31e1313550db561deb4425dddc560f1c8f56b
jdbrandt <jdbrandt@stu.naperville203.org>
2016-04-01 18:34:21 -0500

    Updated comments on Game

--- tcgsim/Game.java
+++ tcgsim/Game.java
@@ -30,12 +30,32 @@ public class Game
 
     private boolean checkDeckSize()
     {
-        return p1.getDeck().size() == 60 && p2.getDeck().size() == 60;
+       //allows for easily adjustable formats
+       int properDeckSize = 60;
+       
+       
+       //I know I could just do return condition1 && condition2, but this seems more obvious to me
+       
+       if (p1.getDeck().size() == properDeckSize && p2.getDeck().size() == properDeckSize) 
+       {
+           return true;
+       }
+       
+       else 
+       {
+           return false;
+       }
     }
-
+    
+    /**
+     * Purpose: Make sure each deck has strictly less than 5 of the same card (b/c (n<5) <==> !(n>4) if n is an element of set of integers)  
+     * Future todos: Possibly change return type from boolean to int to allow for different errors (0 if everything's hunky-dory, 1 if p1 failed, 2 if p2 failed)? Maybe split
+     * into 2 method calls, where check4Cards takes an ArrayList<Cards>?
+     */
+    
     private boolean check4Cards()
     {
-
+        
         for (Card c1 : p1.getDeck())
         {
             int counter = 0;
